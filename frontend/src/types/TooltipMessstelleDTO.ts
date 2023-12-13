f911567ef99912ea36b426026273b82dcea0b842
DanielOber <145556033+DanielOber@users.noreply.github.com>
2023-12-13 18:26:58 +0100

    init

---
+++ frontend/src/types/TooltipMessstelleDTO.ts
@@ -0,0 +1,9 @@
+export default interface TooltipMessstelleDTO {
+    mstId: string;
+    standortDatenportal: string;
+    stadtbezirk: number;
+    realisierungsdatum: string;
+    abbaudatum: string;
+    datumLetzteMessung: string;
+    kfz: boolean;
+}
