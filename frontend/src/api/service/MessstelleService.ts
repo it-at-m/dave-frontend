896e9ff137ca229c25a4ef10ab9592ba1c1e671d
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-01-19 13:20:12 +0100

    testwerte für plausible messtage hinzugefügt

--- frontend/src/api/service/MessstelleService.ts
+++ frontend/src/api/service/MessstelleService.ts
@@ -10,4 +10,21 @@ export default class MessstelleService {
             "Beim Holen der Messstelle ist ein Fehler aufgetreten."
         );
     }
+
+    static getPlausibleMesstage(id: string): string[] {
+        return [
+            "2023-01-12",
+            "2023-02-15",
+            "2023-03-21",
+            "2023-04-08",
+            "2023-05-02",
+            "2023-06-19",
+            "2023-07-04",
+            "2023-08-22",
+            "2023-09-10",
+            "2023-10-24",
+            "2023-11-05",
+            "2023-12-17",
+        ];
+    }
 }
