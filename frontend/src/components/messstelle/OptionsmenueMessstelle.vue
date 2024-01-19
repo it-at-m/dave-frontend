3ddcd165ff2328affb5bdd30bcdca4a79cee90b8
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-01-19 13:19:08 +0100

    Optionsmenue erstellt

---
+++ frontend/src/components/messstelle/OptionsmenueMessstelle.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-container fluid>
+        <v-btn
+            color="secondary"
+            @click="dialog = true"
+        >
+            <v-icon left>mdi-filter-outline</v-icon>
+            <span class="hidden-lg-and-down"
+                >Filtereinstellungen bearbeiten</span
+            >
+            <span class="hidden-xl-only">Filtereinstellungen</span>
+        </v-btn>
+        <v-dialog
+            v-model="dialog"
+            max-width="900px"
+            ><v-card
+                width="900px"
+                flat
+            >
+                <v-card-title>
+                    <v-icon left>mdi-filter-outline</v-icon>
+                    Filtereinstellungen
+                </v-card-title>
+                <v-card-text>
+                    <v-sheet class="overflow-y-auto">
+                        <v-expansion-panels
+                            hover
+                            focusable
+                        >
+                            <zeit-panel :dates="dates" />
+                        </v-expansion-panels>
+                    </v-sheet>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+    </v-container>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import ZeitPanel from "@/components/messstelle/ZeitPanel.vue";
+import MessstelleService from "@/api/service/MessstelleService";
+const dialog = ref(false);
+
+const dates = MessstelleService.getPlausibleMesstage("testid");
+</script>
