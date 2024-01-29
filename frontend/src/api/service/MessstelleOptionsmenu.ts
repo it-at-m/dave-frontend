14f0c7be2baee2d3f5f52cfd8a6995d297bba76f
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-01-29 13:20:29 +0100

    name und url an backend angepasst

--- frontend/src/api/service/MessstelleOptionsmenu.ts
+++ frontend/src/api/service/MessstelleOptionsmenu.ts
@@ -1,15 +1,14 @@
-import SucheComplexSuggestsDTO from "@/types/suche/SucheComplexSuggestsDTO";
 import FetchService from "@/api/service/FetchService";
 import NichtPlausibleTageDTO from "@/types/NichtPlausibleTageDTO";
 
-export default class TagesaggregatMessquerschnitt {
+export default class MessstelleOptionsmenu {
     private static readonly ENDPOINT: string = "api/dave-backend-service";
 
     static getNichtPlausibleTage(
         messquerschnittId: string
     ): Promise<NichtPlausibleTageDTO> {
         return FetchService.getData(
-            `${this.ENDPOINT}/tagesaggregat/nichtPlausibleTage?messquerschnittId=${messquerschnittId}`,
+            `${this.ENDPOINT}/messstelleOptionsmenu/nichtPlausibleTage?messstelle_id=${messquerschnittId}`,
             "Beim Lesen der nicht plausiblen Tage ist ein Fehler aufgetreten."
         );
     }
