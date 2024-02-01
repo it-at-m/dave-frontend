5a51f48c8bab5c15cac5bb3dbb97210312c487ec
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-02-01 14:39:01 +0100

    has autorities überprüft nun ob authorities vorhanden isnd

--- frontend/src/store/modules/user.ts
+++ frontend/src/store/modules/user.ts
@@ -32,14 +32,16 @@ export default {
                 rolePoweruser
             );
         },
-        hasAnyRole(state: any): boolean {
+        hasAuthorities(state: any): boolean {
             return (
-                state.ssoUserInfoResponse.authorities.includes(rolePoweruser) ||
-                state.ssoUserInfoResponse.authorities.includes(roleAnwender)
+                state.ssoUserInfoResponse.authorities != undefined &&
+                (state.ssoUserInfoResponse.authorities.includes(
+                    rolePoweruser
+                ) ||
+                    state.ssoUserInfoResponse.authorities.includes(
+                        roleAnwender
+                    ))
             );
-        },
-        possibleRoles(): Array<string> {
-            return [roleAnwender, rolePoweruser];
         },
     },
     mutations: {
