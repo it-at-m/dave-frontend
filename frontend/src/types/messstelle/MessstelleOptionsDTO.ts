a16c5662f0ca057014bdb6bdbdfe0db35d88d936
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-02-19 11:01:01 +0100

    dto angepasst

--- frontend/src/types/messstelle/MessstelleOptionsDTO.ts
+++ frontend/src/types/messstelle/MessstelleOptionsDTO.ts
@@ -1,6 +1,11 @@
 import FahrzeugOptions from "@/types/messstelle/FahrzeugOptions";
+import ZaehldatenIntervall from "@/types/enum/ZaehldatenIntervall";
 
 export default interface MessstelleOptionsDTO {
     zeitraum: string[];
     fahrzeuge: FahrzeugOptions;
+    zeitauswahl: string;
+    zeitblock: string;
+    wochentag: string;
+    intervall: ZaehldatenIntervall;
 }
