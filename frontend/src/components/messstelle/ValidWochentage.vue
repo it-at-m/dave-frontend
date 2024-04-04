84832e0000fe9e84ad8eaf5cfe7d7395e661215f
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-04-04 09:44:07 +0200

    v-menu eingfügt

--- frontend/src/components/messstelle/ValidWochentage.vue
+++ frontend/src/components/messstelle/ValidWochentage.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>test</div>
+    <div>
+        <v-menu offset-x>
+            <template #activator="{ on, props }">
+                <v-btn
+                    v-bind="props"
+                    width="100%"
+                    elevation="0"
+                    v-on="on"
+                >
+                    test
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item>
+                    <v-list-item-title>test</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -9,6 +27,7 @@ import MessstelleOptionsmenuService from "@/api/service/MessstelleOptionsmenuSer
 import { ValidWochentageInPeriodRequestDto } from "@/types/messstelle/ValidWochentageInPeriodRequestDto";
 import { ValidWochentageInPeriodDto } from "@/types/messstelle/ValidWochentageInPeriodDto";
 import { valid } from "semver";
+import TagesTyp from "@/types/enum/TagesTyp";
 
 const store = useStore();
 const numberValidWochentage = ref({} as ValidWochentageInPeriodDto);
@@ -25,12 +44,39 @@ const validWochentageRequestDto = computed(() => {
     };
 });
 
+const getChosenWochentageNumber = computed(() => {
+    switch (chosenOptions.value.tagesTyp) {
+        case TagesTyp.MO_SO: {
+            return numberValidWochentage.value.numberOfValidTagesTypMoSo;
+        }
+        case TagesTyp.SAMSTAG: {
+            return numberValidWochentage.value.numberOfValidTagesTypMoSo;
+        }
+        case TagesTyp.SONNTAG_FEIERTAG: {
+            return numberValidWochentage.value
+                .numberOfValidTagesTypSonntagFeiertag;
+        }
+        case TagesTyp.WERKTAG_DI_MI_DO: {
+            return numberValidWochentage.value.numberOfValidTagesTypDiMiDo;
+        }
+        case TagesTyp.WERKTAG_MO_FR: {
+            return numberValidWochentage.value.numberOfValidTagesTypMoFr;
+        }
+        case TagesTyp.WERKTAG_FERIEN: {
+            return numberValidWochentage.value
+                .numberOfValidTagesTypWerktagFerien;
+        }
+        default: {
+            return "";
+        }
+    }
+});
+
 watch(chosenOptions, () => {
-    console.log("dont");
-    /* MessstelleOptionsmenuService.getValidWochentageInPeriod(
+    MessstelleOptionsmenuService.getValidWochentageInPeriod(
         validWochentageRequestDto.value
     ).then((response) => {
         numberValidWochentage.value = response;
-    });*/
+    });
 });
 </script>
