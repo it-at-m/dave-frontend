d04934e0158f42cc13d507596d128a690dbfb65d
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-05-07 10:34:59 +0200

    Refactor/dave 254 umstellung composition api views (#117)
    * umstellung composition api
    
    * main also ausnahme für single-world-filename hinzgefügt
    
    * umstellung auf composition api
    
    * ZaehlstelleHeaderDTO create default hinzugefügt

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -66,13 +66,14 @@
     </v-sheet>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { Component, Ref, Vue } from "vue-property-decorator";
 import ZaehlstelleMap from "@/components/map/ZaehlstelleMap.vue";
 
 import domtoimage from "dom-to-image-more";
 import ImageAsset from "@/types/pdfreport/assets/ImageAsset";
 /* eslint-disable no-unused-vars */
+import { ref, onMounted, computed } from "vue";
 import ZaehlstelleKarteDTO from "@/types/zaehlstelle/ZaehlstelleKarteDTO";
 import TooltipZaehlstelleDTO from "@/types/TooltipZaehlstelleDTO";
 import AnzeigeKarteDTO from "@/types/AnzeigeKarteDTO";
@@ -80,154 +81,148 @@ import MessstelleKarteDTO from "@/types/MessstelleKarteDTO";
 import TooltipMessstelleDTO from "@/types/TooltipMessstelleDTO";
 import { messstelleStatusText } from "@/types/enum/MessstelleStatus";
 import DaveUtils from "@/util/DaveUtils";
+import { useStore } from "@/api/util/useStore";
 /* eslint-enable no-unused-vars */
 
-@Component({
-    components: { ZaehlstelleMap },
-})
-export default class App extends Vue {
-    @Ref("map") readonly map!: any;
+const store = useStore();
+const map = ref<null | ZaehlstelleMap>(null);
+const fab = ref(false);
+const creatingPicture = ref(false);
+const printingSearchResult = ref(false);
 
-    fab = false;
-    creatingPicture = false;
-    printingSearchResult = false;
+onMounted(() => {
+    window.scrollTo(0, 0);
+});
 
-    mounted() {
-        window.scrollTo(0, 0);
-    }
-
-    takePicture(): void {
-        this.creatingPicture = true;
-
+function takePicture() {
+    if (map.value != null) {
+        creatingPicture.value = true;
         domtoimage
-            .toJpeg(this.map.$el, { quality: 0.95, cacheBust: true })
+            .toJpeg(map.value.$el, { quality: 0.95, cacheBust: true })
             .then((dataUrl: string) => {
                 const image = new ImageAsset("Hauptkarte", dataUrl);
                 image.width = 100;
-                this.$store.dispatch("addAsset", image);
+                store.dispatch("addAsset", image);
             })
             .finally(() => {
-                this.creatingPicture = false;
+                creatingPicture.value = false;
             });
     }
-
-    /**
-     * Erzeugt aus dem Suchergebnis eine CSV-Datei und
-     * bietet diese als Download an
-     */
-    printSearchResult(): void {
-        this.printingSearchResult = true;
-        const searchResult: Array<AnzeigeKarteDTO> = this.getSearchResult;
-        const searchQuery: string = this.getSearchQuery;
-
-        const zaehlstellenResult = searchResult.filter(
-            (value: AnzeigeKarteDTO) => value.type === "zaehlstelle"
-        ) as Array<ZaehlstelleKarteDTO>;
-
-        const messstellenResult = searchResult.filter(
-            (value: AnzeigeKarteDTO) => value.type === "messstelle"
-        ) as Array<MessstelleKarteDTO>;
-
-        if (zaehlstellenResult.length > 0) {
-            this.printZaehlstellenOfSearchResult(
-                zaehlstellenResult,
-                searchQuery
-            );
-        }
-        if (messstellenResult.length > 0) {
-            this.printMessstellenOfSearchResult(messstellenResult, searchQuery);
-        }
-
-        this.printingSearchResult = false;
-    }
-
-    printZaehlstellenOfSearchResult(
-        searchResult: Array<ZaehlstelleKarteDTO>,
-        searchQuery: string
-    ): void {
-        let filenamePrefix = "Alle_Zaehlstellen";
-        if (searchQuery && searchQuery.trim() !== "") {
-            filenamePrefix = `Zaehlstellen_${searchQuery.replace(/\s/g, "_")}`;
-        }
-        const filename = `${filenamePrefix}_Suchergebnis.csv`;
-
-        const searchResultAsCsvString: Array<string> = [];
-        // Suchbegriff hinzufügen
-        searchResultAsCsvString.push(`Suchbegriff: ${searchQuery}`);
-        // Headerzeile hinzufügen
-        searchResultAsCsvString.push(
-            "Zählstellennummer;Längengrad;Breitengrad;Stadtbezirksnummer;Stadtbezirk;Kreuzungsname;Anzahl der Zählungen;Datum der letzten Zählung"
-        );
-
-        // Baut für jede Zählstelle auf der karte eine eigene Zeile in der CSV zusammen
-        searchResult.forEach((element: ZaehlstelleKarteDTO) => {
-            const tooltip: TooltipZaehlstelleDTO = element.tooltip;
-            const elementAsCsvString: Array<string> = [];
-            elementAsCsvString.push(tooltip.zaehlstellennnummer);
-            elementAsCsvString.push(element.longitude.toString());
-            elementAsCsvString.push(element.latitude.toString());
-            elementAsCsvString.push(`${tooltip.stadtbezirknummer}`);
-            elementAsCsvString.push(tooltip.stadtbezirk);
-            elementAsCsvString.push(tooltip.kreuzungsname);
-            elementAsCsvString.push(`${tooltip.anzahlZaehlungen}`);
-            elementAsCsvString.push(tooltip.datumLetzteZaehlung);
-            searchResultAsCsvString.push(elementAsCsvString.join(";"));
-        });
-        DaveUtils.downloadCsv(searchResultAsCsvString.join("\n"), filename);
-    }
-
-    printMessstellenOfSearchResult(
-        searchResult: Array<MessstelleKarteDTO>,
-        searchQuery: string
-    ): void {
-        let filenamePrefix = "Alle_Messstellen";
-        if (searchQuery && searchQuery.trim() !== "") {
-            filenamePrefix = `Messstellen_${searchQuery.replace(/\s/g, "_")}`;
-        }
-        const filename = `${filenamePrefix}_Suchergebnis.csv`;
-
-        const searchResultAsCsvString: Array<string> = [];
-        // Suchbegriff hinzufügen
-        searchResultAsCsvString.push(`Suchbegriff: ${searchQuery}`);
-        // Headerzeile hinzufügen
-        searchResultAsCsvString.push(
-            "ID Messstelle;Längengrad;Breitengrad;Stadtbezirksnummer;Stadtbezirk;Standort MS;Status;Datum Aufbau;Datum Abbau;Letzter plausibler Messtag"
-        );
-
-        // Baut für jede Zählstelle auf der karte eine eigene Zeile in der CSV zusammen
-        searchResult.forEach((element: MessstelleKarteDTO) => {
-            const tooltip: TooltipMessstelleDTO = element.tooltip;
-            const elementAsCsvString: Array<string> = [];
-            elementAsCsvString.push(tooltip.mstId);
-            elementAsCsvString.push(`${element.longitude}`);
-            elementAsCsvString.push(`${element.latitude}`);
-            elementAsCsvString.push(`${tooltip.stadtbezirknummer}`);
-            elementAsCsvString.push(tooltip.stadtbezirk);
-            elementAsCsvString.push(tooltip.standort);
-            const statusAsText = messstelleStatusText.get(element.status);
-            if (statusAsText) {
-                elementAsCsvString.push(statusAsText);
-            } else {
-                elementAsCsvString.push(element.status);
-            }
-            elementAsCsvString.push(tooltip.realisierungsdatum);
-            elementAsCsvString.push(tooltip.abbaudatum);
-            elementAsCsvString.push(tooltip.datumLetztePlausibleMessung);
-            searchResultAsCsvString.push(elementAsCsvString.join(";"));
-        });
-        DaveUtils.downloadCsv(searchResultAsCsvString.join("\n"), filename);
-    }
-
-    get getSearchResult(): AnzeigeKarteDTO[] {
-        return this.$store.getters["search/result"];
-    }
-
-    get getSearchQuery(): string {
-        return this.$store.getters["search/lastSearchQuery"];
-    }
-
-    get fabColor(): string {
-        return this.fab ? "grey darken-1" : "secondary";
-    }
 }
+
+/**
+ * Erzeugt aus dem Suchergebnis eine CSV-Datei und
+ * bietet diese als Download an
+ */
+function printSearchResult(): void {
+    printingSearchResult.value = true;
+    const searchResult: Array<AnzeigeKarteDTO> = getSearchResult.value;
+    const searchQuery: string = getSearchQuery.value;
+
+    const zaehlstellenResult = searchResult.filter(
+        (value: AnzeigeKarteDTO) => value.type === "zaehlstelle"
+    ) as Array<ZaehlstelleKarteDTO>;
+
+    const messstellenResult = searchResult.filter(
+        (value: AnzeigeKarteDTO) => value.type === "messstelle"
+    ) as Array<MessstelleKarteDTO>;
+
+    if (zaehlstellenResult.length > 0) {
+        printZaehlstellenOfSearchResult(zaehlstellenResult, searchQuery);
+    }
+    if (messstellenResult.length > 0) {
+        printMessstellenOfSearchResult(messstellenResult, searchQuery);
+    }
+
+    printingSearchResult.value = false;
+}
+
+function printMessstellenOfSearchResult(
+    searchResult: Array<MessstelleKarteDTO>,
+    searchQuery: string
+): void {
+    let filenamePrefix = "Alle_Messstellen";
+    if (searchQuery && searchQuery.trim() !== "") {
+        filenamePrefix = `Messstellen_${searchQuery.replace(/\s/g, "_")}`;
+    }
+    const filename = `${filenamePrefix}_Suchergebnis.csv`;
+
+    const searchResultAsCsvString: Array<string> = [];
+    // Suchbegriff hinzufügen
+    searchResultAsCsvString.push(`Suchbegriff: ${searchQuery}`);
+    // Headerzeile hinzufügen
+    searchResultAsCsvString.push(
+        "ID Messstelle;Längengrad;Breitengrad;Stadtbezirksnummer;Stadtbezirk;Standort MS;Status;Datum Aufbau;Datum Abbau;Letzter plausibler Messtag"
+    );
+
+    // Baut für jede Zählstelle auf der karte eine eigene Zeile in der CSV zusammen
+    searchResult.forEach((element: MessstelleKarteDTO) => {
+        const tooltip: TooltipMessstelleDTO = element.tooltip;
+        const elementAsCsvString: Array<string> = [];
+        elementAsCsvString.push(tooltip.mstId);
+        elementAsCsvString.push(`${element.longitude}`);
+        elementAsCsvString.push(`${element.latitude}`);
+        elementAsCsvString.push(`${tooltip.stadtbezirknummer}`);
+        elementAsCsvString.push(tooltip.stadtbezirk);
+        elementAsCsvString.push(tooltip.standort);
+        const statusAsText = messstelleStatusText.get(element.status);
+        if (statusAsText) {
+            elementAsCsvString.push(statusAsText);
+        } else {
+            elementAsCsvString.push(element.status);
+        }
+        elementAsCsvString.push(tooltip.realisierungsdatum);
+        elementAsCsvString.push(tooltip.abbaudatum);
+        elementAsCsvString.push(tooltip.datumLetztePlausibleMessung);
+        searchResultAsCsvString.push(elementAsCsvString.join(";"));
+    });
+    DaveUtils.downloadCsv(searchResultAsCsvString.join("\n"), filename);
+}
+
+function printZaehlstellenOfSearchResult(
+    searchResult: Array<ZaehlstelleKarteDTO>,
+    searchQuery: string
+): void {
+    let filenamePrefix = "Alle_Zaehlstellen";
+    if (searchQuery && searchQuery.trim() !== "") {
+        filenamePrefix = `Zaehlstellen_${searchQuery.replace(/\s/g, "_")}`;
+    }
+    const filename = `${filenamePrefix}_Suchergebnis.csv`;
+
+    const searchResultAsCsvString: Array<string> = [];
+    // Suchbegriff hinzufügen
+    searchResultAsCsvString.push(`Suchbegriff: ${searchQuery}`);
+    // Headerzeile hinzufügen
+    searchResultAsCsvString.push(
+        "Zählstellennummer;Längengrad;Breitengrad;Stadtbezirksnummer;Stadtbezirk;Kreuzungsname;Anzahl der Zählungen;Datum der letzten Zählung"
+    );
+
+    // Baut für jede Zählstelle auf der karte eine eigene Zeile in der CSV zusammen
+    searchResult.forEach((element: ZaehlstelleKarteDTO) => {
+        const tooltip: TooltipZaehlstelleDTO = element.tooltip;
+        const elementAsCsvString: Array<string> = [];
+        elementAsCsvString.push(tooltip.zaehlstellennnummer);
+        elementAsCsvString.push(element.longitude.toString());
+        elementAsCsvString.push(element.latitude.toString());
+        elementAsCsvString.push(`${tooltip.stadtbezirknummer}`);
+        elementAsCsvString.push(tooltip.stadtbezirk);
+        elementAsCsvString.push(tooltip.kreuzungsname);
+        elementAsCsvString.push(`${tooltip.anzahlZaehlungen}`);
+        elementAsCsvString.push(tooltip.datumLetzteZaehlung);
+        searchResultAsCsvString.push(elementAsCsvString.join(";"));
+    });
+    DaveUtils.downloadCsv(searchResultAsCsvString.join("\n"), filename);
+}
+
+const getSearchQuery = computed(() => {
+    return store.getters["search/lastSearchQuery"];
+});
+
+const getSearchResult = computed(() => {
+    return store.getters["search/result"];
+});
+
+const fabColor = computed(() => {
+    return fab.value ? "grey darken-1" : "secondary";
+});
 </script>
