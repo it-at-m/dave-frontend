aff5c1689f0bad33e3a56e74f2dc071389b6dd1a
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-09-04 11:36:10 +0200

    import as type

--- frontend/src/types/messstelle/auswertung/MessstelleAuswertungDTO.ts
+++ frontend/src/types/messstelle/auswertung/MessstelleAuswertungDTO.ts
@@ -1,6 +1,5 @@
 import type BaseEntity from "@/types/common/BaseEntity";
-
-import MessquerschnittAuswertungDTO from "@/types/messstelle/auswertung/MessquerschnittAuswertungDTO";
+import type MessquerschnittAuswertungDTO from "@/types/messstelle/auswertung/MessquerschnittAuswertungDTO";
 
 export default interface MessstelleAuswertungDTO extends BaseEntity {
   mstId: string;
