72998b8b5f20e0326e6c0cff4e66990f3583e635
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-11-12 16:11:43 +0100

    adapt date picker validation

--- frontend/src/components/common/DatePicker.vue
+++ frontend/src/components/common/DatePicker.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-row class="pl-1 ma-0">
+  <v-row class="pl-0 ma-0">
     <v-field-label>
       {{ props.label }}
     </v-field-label>
   </v-row>
-  <v-row class="mb-3">
+  <v-row :class="dateValidationMessage.length > 0 ? 'mb-0' : 'mb-3'">
     <v-col class="my-0" cols="12">
       <!-- https://vue3datepicker.com/ -->
       <vue-date-picker
@@ -22,11 +22,20 @@
       />
     </v-col>
   </v-row>
+  <div
+      v-if="dateValidationMessage.length > 0"
+      class="pl-2 pt-0 mt-0 mb-3 text-body-2">
+      {{ dateValidationMessage}}
+  </div>
 </template>
 
 <script setup lang="ts">
 import VueDatePicker, {type GeneralConfig} from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
+import {useDateUtils} from "@/util/DateUtils";
+import {computed} from "vue";
+import _ from "lodash";
+import moment from "moment";
 
 interface Props {
   label?: string; // Bezeichnung des Datumsfelds
@@ -35,6 +44,12 @@ interface Props {
   minDate: string; // Ob das Datumsfeld deaktiviert sein soll
   maxDate: string; // Ob das Datumsfeld deaktiviert sein soll
 }
+
+const options : Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+};
 
 const props = withDefaults(defineProps<Props>(), {
   label: "",
@@ -58,11 +73,20 @@ const GENERAL_DATE_PICKER_CONFIG: GeneralConfig = {
 
 // https://vue3datepicker.com/props/formatting/#format
 const format = (date: Date) => {
-  const options : Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  };
   return date.toLocaleDateString('de-DE', options);
 };
+
+const dateValidationMessage = computed<string>(() => {
+  let validationMessage = "";
+  if (!_.isNil(date.value) && moment.utc(date.value).isValid()) {
+    const dateToCheck = date.value.toLocaleDateString('de-DE', options);
+    const isDateBetween = useDateUtils().isDateBetweenAsStrings(
+        useDateUtils().formatDateAsStringToISO(dateToCheck),
+        props.minDate,
+        props.maxDate
+    );
+    validationMessage = isDateBetween ? "" : "Das eingegebene Datum liegt außerhalb des gültigen Bereichs.";
+  }
+  return validationMessage;
+})
 </script>
