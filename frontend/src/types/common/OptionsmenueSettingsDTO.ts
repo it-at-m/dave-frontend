98ef9aaa636e2d9985a1959348e80318882a550d
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2025-01-15 13:01:07 +0100

    add optionsmenusettings

---
+++ frontend/src/types/common/OptionsmenueSettingsDTO.ts
@@ -0,0 +1,36 @@
+import Fahrzeugklasse from "../enum/Fahrzeugklasse";
+import ZaehldatenIntervall from "../enum/ZaehldatenIntervall";
+
+export default interface OptionsmenueSettingsDTO {
+
+    fahrzeugklasse:  Fahrzeugklasse;
+
+    intervall: ZaehldatenIntervall;
+
+    kraftfahrzeugverkehrChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    schwerverkehrChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    gueterverkehrChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    schwerverkehrsanteilProzentChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    gueterverkehrsanteilProzentChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    radverkehrChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    fussverkehrChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    lastkraftwagenChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    lastzuegeChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    busseChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    kraftraederChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    personenkraftwagenChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+    lieferwagenChoosableIntervals: Array<ZaehldatenIntervall> | undefined;
+
+}
