c9093dc2ca56c440678d847292a3ba5640199657
alexander.kerscher <alexander.kerscher@muenchen.de>
2025-01-21 08:08:58 +0100

    prettier

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -84,12 +84,12 @@ import InfoMessage from "@/components/app/InfoMessage.vue";
 import SearchInputField from "@/components/app/SearchInputField.vue";
 import VisitHistory from "@/components/app/VisitHistory.vue";
 import TheSnackbar from "@/components/common/TheSnackbar.vue";
+import { useMapOptionsStore } from "@/store/MapOptionsStore";
 import { useSearchStore } from "@/store/SearchStore";
 import { useSnackbarStore } from "@/store/SnackbarStore";
 import { useUserStore } from "@/store/UserStore";
 import SsoUserInfoResponse from "@/types/app/SsoUserInfoResponse";
 import VersionInfoResponse from "@/types/app/VersionInfoResponse";
-import {useMapOptionsStore} from "@/store/MapOptionsStore";
 
 const URL_HANDBUCH_LINK =
   "https://wilma.muenchen.de/web/senders/af10dc2a-8da5-4d24-815a-b6a9df4c686b/documents/330c5be9-2ee3-4438-8623-6557755260d3";
@@ -176,7 +176,6 @@ function shortCuts(event: KeyboardEvent) {
 function resetMapAndSearch() {
   mapOptionsStore.resetMapOptions();
   searchStore.resetAndTriggerSearch();
-
 }
 </script>
 
