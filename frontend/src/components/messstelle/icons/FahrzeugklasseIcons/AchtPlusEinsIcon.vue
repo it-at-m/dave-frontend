029cb850b73156919410c4e8219f8598a2b2606c
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2025-06-27 12:28:19 +0200

    reformat code

--- frontend/src/components/messstelle/icons/FahrzeugklasseIcons/AchtPlusEinsIcon.vue
+++ frontend/src/components/messstelle/icons/FahrzeugklasseIcons/AchtPlusEinsIcon.vue
@@ -1,14 +1,20 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg"
-       width="32"
-       height="32"
-       viewBox="0 0 32 32"
-       class="mt-n2">
-    <text y="16" class="svg-text" fill="currentColor">8+1</text>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+  >
+    <text
+      y="16"
+      class="svg-text"
+      fill="currentColor"
+    >
+      8+1
+    </text>
   </svg>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <style lang="css">
 .svg-text {
   font: 16px Arial;
