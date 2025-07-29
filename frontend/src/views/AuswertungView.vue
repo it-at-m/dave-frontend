4b062821d4954dd514fdeb85f145a1e324e96137
boal <56300801+boal@users.noreply.github.com>
2025-07-29 14:38:23 +0200

    DAVE-717  Entfernen Divider in Gesamtauswertung und hinzufügen rechter Border (#482)
    * add right border to stepper menu and remove vertical devider
    
    * adapt stepper height calc method
    
    * reformat code

--- frontend/src/views/AuswertungView.vue
+++ frontend/src/views/AuswertungView.vue
@@ -7,6 +7,11 @@
       >
         <v-sheet
           class="d-flex flex-column overflow-y-auto"
+          style="
+            border-right-color: lightgrey;
+            border-right-style: solid;
+            border-right-width: 1px;
+          "
           :height="stepperSheetHeight"
           width="100%"
         >
@@ -37,7 +42,6 @@
           </v-card-actions>
         </v-sheet>
       </v-col>
-      <v-divider vertical />
       <v-col cols="8">
         <progress-loader v-model="chartDataLoading" />
         <step-line-card
@@ -214,8 +218,7 @@ const appBarHeight = computed(() => {
 });
 
 const stepperSheetHeight = computed(() => {
-  const overflowX = display.width.value / 3 <= minWidth;
-  return 100 - (overflowX ? 3 : 0) - appBarHeight.value + "vh";
+  return 100 - appBarHeight.value + "vh";
 });
 
 const stepperSheetActionsHeight = computed(() => {
