fbbd8b2ee3ba92057444e48f589bb700a94692a5
martind260 <99957516+martind260@users.noreply.github.com>
2026-01-26 09:26:44 +0100

    Bereitstellen-der-stadtbezogenen-Texte-per-Konfiguration_Datenportal

--- frontend/src/types/configuration/ConfigurationDTO.ts
+++ frontend/src/types/configuration/ConfigurationDTO.ts
@@ -1,7 +1,7 @@
-import type MapConfigurationDTO from "@/types/configuration/MapConfigurationDTO";
 import type ZaehlstelleConfigurationDTO from "@/types/configuration/ZaehlstelleConfigurationDTO";
+import type TenantConfigurationDTO from "@/types/configuration/TenantConfigurationDTO";
 
 export default interface ConfigurationDTO {
-  map: MapConfigurationDTO;
   zaehlstelle: ZaehlstelleConfigurationDTO;
+  tenant: TenantConfigurationDTO;
 }
