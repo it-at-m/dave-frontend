f23a909dea4df96029b38929dec3d990a144d63a
Angelina Wiedemann <angelina.wiedemann@muenchen.de>
2026-01-30 11:30:20 +0100

    Bewegungsrichtung eingefuegt

---
+++ frontend/src/types/enum/Bewegungsrichtung.ts
@@ -0,0 +1,22 @@
+enum Bewegungsrichtung {
+    EIN = "EIN",
+    AUS = "AUS",
+}
+
+export default Bewegungsrichtung;
+
+export const himmelsRichtungenTextLong: Map<string, string> = new Map<
+    string,
+    string
+>([
+    [Bewegungsrichtung.EIN, "Ein"],
+    [Bewegungsrichtung.AUS, "Aus"],
+]);
+
+export const himmelsRichtungenTextShort: Map<string, string> = new Map<
+    string,
+    string
+>([
+    [Bewegungsrichtung.EIN, "Ein"],
+    [Bewegungsrichtung.AUS, "Aus"],
+]);
