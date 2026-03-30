8ed50cff567de9c128e35d91856f7c67bdc9d26a
Marina Schmoll <marina.schmoll@muenchen.de>
2026-03-30 13:34:46 +0200

    fix bug zeitauswahl block

--- frontend/src/components/zaehlstelle/optionsmenue/panels/ZeitauswahlPanel.vue
+++ frontend/src/components/zaehlstelle/optionsmenue/panels/ZeitauswahlPanel.vue
@@ -36,9 +36,7 @@
                 <v-radio
                   label="Tageswert"
                   :value="Zeitauswahl.TAGESWERT"
-                  :disabled="
-                    isTeilzaehlungFussverkehr || isOnlyFussgaengerSelected
-                  "
+                  :disabled="isTeilzaehlung && isOnlyFussgaengerSelected"
                   @mouseover="hoverTageswert = true"
                   @mouseleave="hoverTageswert = false"
                 />
@@ -332,12 +330,8 @@ const zaehldatenIntervalle = computed<Array<KeyVal>>(() => {
   return ZaehldatenIntervallToSelect;
 });
 
-const isTeilzaehlungFussverkehr = computed(() => {
-  return (
-    activeZaehlung.value.kategorien.length === 1 &&
-    activeZaehlung.value.kategorien[0] === Fahrzeug.FUSS &&
-    activeZaehlung.value.zaehldauer !== Zaehldauer.DAUER_24_STUNDEN
-  );
+const isTeilzaehlung = computed(() => {
+  return activeZaehlung.value.zaehldauer !== Zaehldauer.DAUER_24_STUNDEN;
 });
 
 const isOnlyFussgaengerSelected = computed(() => {
@@ -368,8 +362,8 @@ watch(
 function adaptOptionsUpdate() {
   if (
     isOnlyFussgaengerSelected.value &&
-    (chosenOptionsCopy.value.zeitauswahl === Zeitauswahl.TAGESWERT ||
-      chosenOptionsCopy.value.zeitauswahl === Zeitauswahl.SPITZENSTUNDE_KFZ)
+    chosenOptionsCopy.value.zeitauswahl === Zeitauswahl.TAGESWERT &&
+    isTeilzaehlung.value
   ) {
     chosenOptionsCopy.value.zeitauswahl = Zeitauswahl.BLOCK;
     const zbMax =
