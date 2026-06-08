55c67e237ce3d86b8e8f0d94c9b4fecb42a448eb
Anett Hübner <anett_huebner@gmx.de>
2026-06-08 15:39:19 +0200

    corrected Belastungsplan RAD + FUSS

--- frontend/src/components/zaehlstelle/charts/BelastungsplanKreuzungSvg.vue
+++ frontend/src/components/zaehlstelle/charts/BelastungsplanKreuzungSvg.vue
@@ -690,6 +690,21 @@ function calcFahrbeziehungen(data: LadeBelastungsplanDTO) {
         )
           lowestFahrbeziehungsValue.value =
             belastungsplanMethods.positiveNumber(data.value1.values[v][n]);
+
+        if (data.value1.label === "RAD" && data.value2.label === "FUSS") {
+          if (
+            belastungsplanMethods.positiveNumber(data.value1.values[v][n]) + belastungsplanMethods.positiveNumber(data.value2.values[v][n]) >
+            highestFahrbeziehungsValue.value
+          )
+            highestFahrbeziehungsValue.value =
+              belastungsplanMethods.positiveNumber(data.value1.values[v][n]) + belastungsplanMethods.positiveNumber(data.value2.values[v][n]);
+          if (
+            belastungsplanMethods.positiveNumber(data.value1.values[v][n]) + belastungsplanMethods.positiveNumber(data.value2.values[v][n]) <
+            lowestFahrbeziehungsValue.value
+          )
+            lowestFahrbeziehungsValue.value =
+              belastungsplanMethods.positiveNumber(data.value1.values[v][n]) + belastungsplanMethods.positiveNumber(data.value2.values[v][n]);
+        }
         // Fahrbeziehungstyp wird gesetzt um später die Position der Linien berrechnen zu können
         knotenarmVon.addVonFahrbeziehungsType(
           belastungsplanFahrbeziehung.fahrbeziehungsTyp
