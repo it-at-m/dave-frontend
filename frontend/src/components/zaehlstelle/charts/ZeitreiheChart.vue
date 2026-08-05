ccecf2f0bce0f46d773f7060f8caa9b504a9eaf9
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2026-08-05 15:31:45 +0200

    adapt zeitreihe chart

--- frontend/src/components/zaehlstelle/charts/ZeitreiheChart.vue
+++ frontend/src/components/zaehlstelle/charts/ZeitreiheChart.vue
@@ -508,7 +508,7 @@ function fillCsvRow(isWanted: boolean, data: number | null) {
     ) {
       row += ";nicht vorh.";
     } else if (
-      (data == null || data === 0) &&
+      data == null &&
       filterOptions.value.zeitauswahl === Zeitauswahl.TAGESWERT
     ) {
       row += ";Tageswert nicht vorh.";
