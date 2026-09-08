154176737616dd37f2dda36a7eac151b751f7618
jan-niklasml <jan-niklas.mueller@muenchen.de>
2026-09-08 08:31:41 +0200

    check for vbz in watch

--- frontend/src/components/zaehlstelle/optionsmenue/panels/ZaehlungsvergleichPanel.vue
+++ frontend/src/components/zaehlstelle/optionsmenue/panels/ZaehlungsvergleichPanel.vue
@@ -375,6 +375,25 @@ watch(
       // ausgewählte Arm nur bei der Vergleichszählung vorhanden ist
 
       if (
+        chosenOptionsCopy.value.vonKnotenarm != null &&
+        chosenOptionsCopy.value.nachKnotenarm != null
+      ) {
+        if (
+          !activeZaehlung.value.verkehrsbeziehungen.some(
+            (vbz) =>
+              vbz.von === chosenOptionsCopy.value.vonKnotenarm &&
+              vbz.nach === chosenOptionsCopy.value.nachKnotenarm
+          )
+        ) {
+          // Aktuell im Filtermenü gewählte Verkehrsbeziehung ist bei der Ausgangszählung nicht vorhanden
+          chosenOptionsCopy.value.vonKnotenarm = null;
+          chosenOptionsCopy.value.nachKnotenarm = null;
+          chosenOptionsCopy.value.vonIds = [1, 2, 3, 4, 5, 6, 7, 8];
+          chosenOptionsCopy.value.nachIds = [1, 2, 3, 4, 5, 6, 7, 8];
+        }
+      }
+
+      if (
         chosenOptionsCopy.value.vonIds.some(
           (id) =>
             !activeZaehlung.value.knotenarme.some(
