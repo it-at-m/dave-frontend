cff5349c4e83da1b1d12ed67f74d857181f658ae
jan-niklasml <jan-niklas.mueller@muenchen.de>
2026-09-14 12:53:59 +0200

    remember previous selected vergleichszaehlung id

--- frontend/src/components/zaehlstelle/optionsmenue/panels/ZaehlungsvergleichPanel.vue
+++ frontend/src/components/zaehlstelle/optionsmenue/panels/ZaehlungsvergleichPanel.vue
@@ -120,6 +120,7 @@ const dateUtils = useDateUtils();
 
 const vergleichsdatumDifferenzdarstellung = ref(new Array<KeyVal>());
 const vergleichsdatumZeitreihe = ref(new Array<KeyVal>());
+const previousSelectedVergleichszaehlungId = ref();
 
 const hoverSelectBasisdatum = ref(false);
 const hoverSelectVergleichsdatum = ref(false);
@@ -283,11 +284,21 @@ function zeitreihenVergleichsdatumCalculator(): void {
   // Setze idVergleichszaehlungZeitreihe zurück (auf null), wenn der Wert nicht im Array result enthalten ist
   const selectedVergleichszaehlungId =
     chosenOptionsCopy.value.idVergleichszaehlungZeitreihe;
+  const previousSelectedId = previousSelectedVergleichszaehlungId.value;
   if (
     selectedVergleichszaehlungId != null &&
     !result.some((item) => item.value === selectedVergleichszaehlungId)
   ) {
     chosenOptionsCopy.value.idVergleichszaehlungZeitreihe = null;
+  } else if (previousSelectedId == null) {
+    // ID der ausgewählten Vergleichszählung merken
+    previousSelectedVergleichszaehlungId.value = selectedVergleichszaehlungId;
+  } else if (
+    selectedVergleichszaehlungId == null &&
+    result.some((item) => item.value === previousSelectedId)
+  ) {
+    // ID der vorher ausgewählten Vergleichszählung wiederherstellen
+    chosenOptionsCopy.value.idVergleichszaehlungZeitreihe = previousSelectedId;
   }
 }
 
